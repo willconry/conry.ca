2dc735b589e8dcf88996c838ebe8d4bb71848808
Will Conry <will@conry.ca>
2020-11-18 14:53:55 +0000

    Refactor and navbar update

--- src/components/pages/Resume/Resume.js
+++ src/components/pages/Resume/Resume.js
@@ -4,19 +4,19 @@ import './Resume.css'
 
 function Resume() {
     // const url =  "https://cors-anywhere.herokuapp.com/https://github.com/willconry/resume/raw/master/will_conry_resume.pdf"
-    const url =  "https://cors-anywhere.herokuapp.com/https://s3.ca-central-1.amazonaws.com/conry.ca/will_conry_resume.pdf"
+    // const url =  "https://cors-anywhere.herokuapp.com/https://s3.ca-central-1.amazonaws.com/conry.ca/will_conry_resume.pdf"
 
-
-  
     pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`
     // const [numPages, setNumPages] = useState(null)
     // const [pageNumber, setPageNumber] = useState(1)
+
+    const file = "/will_conry_resume.pdf"
     
     return (
         <>
             <div className="resume"> 
                 <div className="resume-container">
-                    <Document file={url} className="resume-preview"> 
+                    <Document file={file} className="resume-preview"> 
                         <Page pageNumber={1} scale={1.5} /> 
                     </Document> 
                 </div>
