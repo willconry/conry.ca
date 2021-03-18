354d0e3769e7a3322074a85c3753004a35445a56
Will Conry <will@conry.ca>
2021-03-18 20:52:29 +0000

    Fixed pdf issue, pdf extension was not included in 'rewrites and redirects'

--- src/components/DisplayPage.js
+++ src/components/DisplayPage.js
@@ -1,17 +1,15 @@
 import React from 'react'
 import { Document, Page } from "react-pdf/dist/esm/entry.webpack";
 import 'react-pdf/dist/esm/Page/AnnotationLayer.css';
+import resume from '../static/will_conry_resume.pdf';
 
 function DisplayPage({url, page}) {
-
-    // pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`
-    const cors = "https://cors-anywhere.herokuapp.com/"
     
     return (
         <>
             <div className="page"> 
-                <div className="page-container">
-                    <Document file={cors + url}> 
+            <div className="page-container">
+                    <Document file={resume}> 
                         <Page className="page-preview" pageNumber={page} width={840} /> 
                     </Document> 
                 </div>
